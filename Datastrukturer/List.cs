4a1f892318f7d88e03fd6714d3d5547e9d4a2672
TClaesborn <142004581+TClaesborn@users.noreply.github.com>
2024-02-07 10:29:40 +0100

    Add project files.

---
+++ Datastrukturer/List.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBrew
+{
+    internal class List<T> // T är en input type.
+    {
+        public Node<T> head { get; private set; }
+
+        public List()
+        {
+            head = null;
+        }
+            
+        public void RemoveFirst()
+        {
+            head = head.next;
+        }
+
+        public bool RemoveValue(T data)
+        { return true; }
+
+        public void AddAtIndex(T data, int index)
+        { }
+
+
+        public void Add(T data)
+        {
+            if (head == null)
+                head = new Node<T>(data); //Kollar och sätter första värdet
+            else
+            {
+                Node<T> current = head; //Om första värde finns så spara en ref till det
+                while(current.next != null) //Kolla om current har ett nästa värde.
+                {
+                    current = current.next; //Flyttar ref till nästa värde
+                }
+                current.next = new Node<T>(data); //Skapar ny nod vid slutet av listan.
+            }
+        }
+        public bool Find(T data)
+        {
+            Node<T> current = head;
+            while(current != null)
+            {
+                if (current.value.Equals(data))
+                    return true;
+                current = current.next;
+            }
+            return false;
+        }
+
+        public void PrintAll()
+        {
+            Node<T> current = head;
+            while(current != null)
+            {
+                Console.WriteLine(current.value);
+                current = current.next;
+            }
+        }
+    }
+
+    internal class Node<T>
+    {
+        public T value { get; set; }
+        public Node<T> next { get; set; }
+
+        public Node(T data)
+        {
+            this.value = data;
+            this.next = null;
+        }
+    }
+}
---
+++ Datastrukturer/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        //Länkad lista.
+        HomeBrew.List<string> list = new();
+        list.Add("Hej");
+        list.Add("Vad");
+        list.Add("Heter");
+        list.Add("Du");
+        list.PrintAll();
+        list.RemoveFirst();
+        list.PrintAll();
+        list.RemoveFirst();
+        list.RemoveFirst();
+        Console.WriteLine(list.Find("Du"));
+    }
+}
